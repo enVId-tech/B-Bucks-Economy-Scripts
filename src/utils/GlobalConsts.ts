fcda6fb8c64b506f029a0c297fc671d9c35f034f
Erick Tran <thelittlebotengineer@gmail.com>
2026-07-27 08:58:51 -0700

    refactor(globalconsts.ts): refactor consts to have explicit type declarations

--- src/utils/GlobalConsts.ts
+++ src/utils/GlobalConsts.ts
@@ -6,28 +6,28 @@
 // This file contains global constants used throughout the B-Bucks Economy Scripts project.
 
 // --- Main Sheet ---
-const USER_STARTING_ROW = 7;
+const USER_STARTING_ROW: number = 7;
 
 // -- columns --
 // this isn't in an object btw bc i'm dumb and also bc i didnt think abt it when starting this project
 // if you are maintaining this in the future, just put it in a key-value pair object
 // also bc im lazy asf so yeah i didnt do it. what are you gonna do abt it
-const BALANCE_COL = 2;
-const EARNINGS_COL = 3;
-const NET_INCOME_COL = 4;
-const EXPENDITURES_COL = 5;
-const INVESTMENT_RETURNS_COL = 6;
+const BALANCE_COL: number = 2;
+const EARNINGS_COL: number = 3;
+const NET_INCOME_COL: number = 4;
+const EXPENDITURES_COL: number = 5;
+const INVESTMENT_RETURNS_COL: number = 6;
 
 // investments
-const INITIAL_DEPOSIT_COL = 7;
-const DATE_DEPOSIT_COL = 8;
-const GROSS_INVESTMENT_GAIN_COL = 9;
-const NET_INVESTMENT_GAIN_COL = 10;
-const NET_PERCENTAGE_GAIN_COL = 11;
+const INITIAL_DEPOSIT_COL: number = 7;
+const DATE_DEPOSIT_COL: number = 8;
+const GROSS_INVESTMENT_GAIN_COL: number = 9;
+const NET_INVESTMENT_GAIN_COL: number = 10;
+const NET_PERCENTAGE_GAIN_COL: number = 11;
 
 // --- Services Sheet ---
-const DEFAULT_SERVICES_SHEET = "Services";
-const SERVICES_ROW_START = 3;
+const DEFAULT_SERVICES_SHEET: string = "Services";
+const SERVICES_ROW_START: number = 3;
 
 const SERVICES_COLUMNS: { [key: string]: number | number[] } = {
     itemName: 1,
@@ -41,11 +41,11 @@ const SERVICES_COLUMNS: { [key: string]: number | number[] } = {
 // --- Settings Sheet ---
 // -- DO NOT CHANGE THESE COLUMNS UNLESS YOU KNOW WHAT YOU ARE DOING --
 // no but really actually dont pls ty, the sheet is named settings cuz the code likes it and ppl are stupid asf :D
-const DEFAULT_SETTINGS_SHEET = "Settings";
-const SETTINGS_ROW_START = 4;
+const DEFAULT_SETTINGS_SHEET: string = "Settings";
+const SETTINGS_ROW_START: number = 4;
 
 // change only if u dont want the default option which is like wai bro ts beautiful
-const SETTINGS_COLUMNS = {
+const SETTINGS_COLUMNS: { [key: string]: { keyCol: number; valCol: number } } = {
     importantDates: { keyCol: 2, valCol: 3 },
     standardPercentages: { keyCol: 5, valCol: 6 },
     mandatedPolicies: { keyCol: 8, valCol: 9 },
@@ -54,16 +54,16 @@ const SETTINGS_COLUMNS = {
 }
 
 // --- Transaction Sheet ---
-const DEFAULT_TRANSACTIONS_SHEET = "Transactions";
-const TRANSACTIONS_ROW_START = 3;
+const DEFAULT_TRANSACTIONS_SHEET: string = "Transactions";
+const TRANSACTIONS_ROW_START: number = 3;
 
 // -- Fill Names Utils ---
-const STARTING_CELL = "A7";
-const SHEET_CELL_NAME = "A2";
+const STARTING_CELL: string = "A7";
+const SHEET_CELL_NAME: string = "A2";
 
 // -- Historical Records Sheet ---
-const DEFAULT_HISTORICAL_RECORDS_SHEET = "Economic Records";
-const HISTORICAL_RECORDS_ROW_START = 11;
+const DEFAULT_HISTORICAL_RECORDS_SHEET: string = "Economic Records";
+const HISTORICAL_RECORDS_ROW_START: number = 11;
 
 
 // -- Timestamp Sheets --
@@ -80,15 +80,16 @@ const TIMESTAMP_LIST: string[] = [
     "Period 10",
     "Template",
     "Testing"
-]
-const TIMESTAMP_CELL = "A5";
+];
+
+const TIMESTAMP_CELL: string = "A5";
 
 // ADVANCED SETTINGS - Don't modify these values unless you know what you're doing (or you're me)
-const WAIT_LOCK_TIME = 5000; // in ms
-const SERVER_SIDE_CACHE_AGE = 21600; // in seconds
+const WAIT_LOCK_TIME: number = 5000; // in ms
+const SERVER_SIDE_CACHE_AGE: number = 21600; // in seconds
 
 // this is separated and not in an object because separate variables are easier to use since these are used pretty often (and are cleaner)
-const SERVICES_CACHED_KEY = "cachedServices";
-const SETTINGS_CACHED_KEY = "cachedSettings";
-const TRANSACTIONS_CACHED_KEY = "cachedTransactions";
-const INVESTMENTS_LEDGER_CACHED_KEY = "cachedInvestmentsLedger";
+const SERVICES_CACHED_KEY: string = "cachedServices";
+const SETTINGS_CACHED_KEY: string = "cachedSettings";
+const TRANSACTIONS_CACHED_KEY: string = "cachedTransactions";
+const INVESTMENTS_LEDGER_CACHED_KEY: string = "cachedInvestmentsLedger";
